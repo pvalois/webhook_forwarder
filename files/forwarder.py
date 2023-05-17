aa67fd0543144221fc9328f15facee4b670fdb71
Pascal VALOIS <pvalois@adlere.priv>
2023-05-17 15:39:58 +0200

    fix: environment variable usage

--- files/forwarder.py
+++ files/forwarder.py
@@ -8,7 +8,7 @@ import os
 ###############################################################################
 
 def send_mattermost(report):
-  mmwebhooks=""
+  mmwebhooks=os.getenv("MM_WEBHOOKS_URL", "https://default-url.com")
   headers = {'Content-Type': 'application/json',}
   values = json.dumps({ "text": report})
   response = requests.post(mmwebhooks, headers=headers, data=values)
@@ -17,9 +17,11 @@ def send_mattermost(report):
 
 app = Flask(__name__)
 
-ip=os.popen('hostname -I').read().strip()
+#ip=os.popen('hostname -I').read().strip()
+ip=os.popen('hostname -I').read().strip().split(" ")[0]
 cluster=ip.split('.')[2][-1]
 
+critical_words=['PVC']
 messages={}
 
 @app.route('/forwarder/', methods=['POST'])
@@ -33,7 +35,13 @@ def api_gh_message():
   for alert in request.json['alerts']:
     desc=alert['annotations']['description']
     h=hash(desc)
-    if (h not in messages or timeinseconds-messages[h]>3600):
+
+    unhold=False
+    for word in critical_words:
+      if (word in desc):
+        unhold=True
+
+    if (h not in messages or timeinseconds-messages[h]>300*86400 or unhold):
       msg=msg+"* "+desc+"\n"
       messages[h]=timeinseconds
       send=1
@@ -45,6 +53,4 @@ def api_gh_message():
 ###############################################################################
 
 if __name__ == '__main__': 
-  app.run(host=ip,port="5000")
-
-
+  app.run(host=ip,port=os.getenv("FLASK_PORT", "5000"))
